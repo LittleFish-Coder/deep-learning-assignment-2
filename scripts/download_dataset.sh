d5aab027727f5c54c3d2b1c1d8ce5c045f7612bd
LittleFish-Coder <adrianyu890317@gmail.com>
2024-05-19 16:51:02 +0800

    add scripts for downloading dataset

---
+++ scripts/download_dataset.sh
@@ -0,0 +1,8 @@
+# get the dataset from the link
+wget https://cchsu.info/files/images.zip
+
+# create a directory to store the dataset
+mkdir -p dataset
+
+# unzip the dataset into the directory
+unzip images.zip -d dataset
