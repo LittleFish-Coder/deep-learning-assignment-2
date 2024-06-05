e230d703052a0fa6b6d7e08bffa4572ea69e1e9b
Chen-Yang Yu <adrianyu890317@gmail.com>
2024-06-05 17:43:54 +0800

    update command

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,8 +1,11 @@
-# get the dataset from the link
-wget https://cchsu.info/files/images.zip
+# This script downloads the dataset from the link and unzips it into the dataset directory.
+echo "Downloading the dataset..."
 
 # create a directory to store the dataset
 mkdir -p dataset
+
+# get the dataset from the link
+wget https://cchsu.info/files/images.zip
 
 # unzip the dataset into the directory
 unzip images.zip -d dataset
