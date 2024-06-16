7103070b256594364825964e28c17be96d24053a
Chen-Yang Yu <adrianyu890317@gmail.com>
2024-06-16 13:15:44 +0800

    add task1

--- models/cnn.py
+++ models/cnn.py
@@ -15,10 +15,13 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, padding=0)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=0)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=0)
         self.norm1 = nn.BatchNorm2d(num_features=32)
         self.norm2 = nn.BatchNorm2d(num_features=64)
+        self.norm3 = nn.BatchNorm2d(num_features=128)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(in_features=64 * 62 * 62, out_features=num_classes)
+        self.fc1 = nn.Linear(in_features=128 * 30 * 30, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=num_classes)
 
     def forward(self, x):
         # 1st convolutional layer
@@ -26,16 +29,28 @@ class CNN(nn.Module):
         x = self.norm1(x)
         x = F.relu(x)
         x = self.pool(x)  # x.shape = (batch_size, 32, 127, 127)
+
         # 2nd convolutional layer
         x = self.conv2(x)  # x.shape = (batch_size, 64, 125, 125)
         x = self.norm2(x)
         x = F.relu(x)
         x = self.pool(x)  # x.shape = (batch_size, 64, 62, 62)
-        # Flatten P.S. x = x.view(x.size(0), -1) also works
-        x = x.reshape(x.shape[0], -1)  # x.shape = (batch_size, 64 * 62 * 62)
+
+        # 3rd convolutional layer
+        x = self.conv3(x)  # x.shape = (batch_size, 128, 60, 60)
+        x = self.norm3(x)
+        x = F.relu(x)
+        x = self.pool(x)  # x.shape = (batch_size, 128, 30, 30)
+
+        # Flatten
+        x = x.view(x.size(0), -1)  # x.shape = (batch_size, 128 * 30 * 30)
+
         # Fully connected layer
         x = self.fc1(x)
-        # we don't apply softmax here because pytorch's cross-entropy loss function implicitly applies softmax
+        x = F.relu(x)
+        x = self.fc2(x)
+
+        # We don't apply softmax here because PyTorch's cross-entropy loss function implicitly applies softmax
         return x
 
 
