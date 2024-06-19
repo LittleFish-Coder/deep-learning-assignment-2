c2165f13d1aa68e9d0178052a7f078e1eb677481
Chen-Yang Yu <adrianyu890317@gmail.com>
2024-06-20 03:51:55 +0800

    change model structure

--- models/attention_cnn.py
+++ models/attention_cnn.py
@@ -118,3 +118,12 @@ class ConvNet(nn.Module):
         x = self.fc(x)
 
         return x
+
+
+if __name__ == "__main__":
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = ConvNet().to(device)
+
+    summary(model, input_size=(3, 256, 256))
--- models/cnn.py
+++ models/cnn.py
@@ -13,46 +13,50 @@ class CNN(nn.Module):
 
     def __init__(self, in_channels=3, num_classes=50):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, padding=0)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=0)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=0)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
         self.norm1 = nn.BatchNorm2d(num_features=32)
         self.norm2 = nn.BatchNorm2d(num_features=64)
         self.norm3 = nn.BatchNorm2d(num_features=128)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(in_features=128 * 30 * 30, out_features=512)
-        self.fc2 = nn.Linear(in_features=512, out_features=num_classes)
+        self.global_avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Linear(in_features=128, out_features=256)
+        self.fc2 = nn.Linear(in_features=256, out_features=num_classes)
         self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
         # 1st convolutional layer
-        x = self.conv1(x)  # x.shape = (batch_size, 32, 254, 254)
+        x = self.conv1(x)  # x.shape = (batch_size, 32, 256, 256)
         x = self.norm1(x)
         x = F.relu(x)
-        x = self.pool(x)  # x.shape = (batch_size, 32, 127, 127)
+        x = self.pool(x)  # x.shape = (batch_size, 32, 128, 128)
 
         # 2nd convolutional layer
-        x = self.conv2(x)  # x.shape = (batch_size, 64, 125, 125)
+        x = self.conv2(x)  # x.shape = (batch_size, 64, 126, 126)
         x = self.norm2(x)
         x = F.relu(x)
-        x = self.pool(x)  # x.shape = (batch_size, 64, 62, 62)
+        x = self.pool(x)  # x.shape = (batch_size, 64, 63, 63)
 
         # 3rd convolutional layer
-        x = self.conv3(x)  # x.shape = (batch_size, 128, 60, 60)
+        x = self.conv3(x)  # x.shape = (batch_size, 128, 61, 61)
         x = self.norm3(x)
         x = F.relu(x)
-        x = self.dropout(x)
         x = self.pool(x)  # x.shape = (batch_size, 128, 30, 30)
 
+        # Global average pooling
+        x = self.global_avg_pool(x)  # x.shape = (batch_size, 128, 1, 1)
+
         # Flatten
-        x = x.view(x.size(0), -1)  # x.shape = (batch_size, 128 * 30 * 30)
+        x = x.view(x.size(0), -1)  # x.shape = (batch_size, 128)
 
         # Fully connected layer
-        x = self.fc1(x)
+        x = self.fc1(x)  # x.shape = (batch_size, 256)
         x = F.relu(x)
+        x = self.dropout(x)
 
         # Output layer
-        x = self.fc2(x)
+        x = self.fc2(x)  # x.shape = (batch_size, num_classes)
 
         # We don't apply softmax here because PyTorch's cross-entropy loss function implicitly applies softmax
         return x
